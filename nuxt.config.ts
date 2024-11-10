76ddcb10efd54be5b8f492b50fe991b4de9d540b
Ivanna Cantelberg <ivannacantelberg@gmail.com>
2024-11-10 13:07:51 +0100

    Added Google  tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  site: { 
+    url: 'https://ukrainianartproject.com', 
+    name: 'Ukrainian art projects' 
+  }, 
+  
+  app: {
+    
+    head: {
+      noscript: [
+        {
+          children: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}"
+                    height="0" width="0" style="display:none;visibility:hidden"></iframe>`, 
+          type: 'text/html',
+          body: true
+        }
+      ], 
+      script: [
+        {
+
+          hid: 'gtm',
+          children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','${process.env.GTM_ID}');`,
+          type: 'text/javascript'
+        }
+      ]
+    }, 
+    
+  }, 
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxtjs/i18n', '@nuxt/content', '@element-plus/nuxt', 'nuxt-og-image',], 
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxtjs/i18n', '@nuxt/content', '@element-plus/nuxt', 'nuxt-og-image'], 
   css: ['~/assets/css/base.less'],
   tailwindcss: {
     cssPath: '~/assets/css/tailwind.less',
